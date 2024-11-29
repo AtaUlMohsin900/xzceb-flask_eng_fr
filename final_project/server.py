6cca419838aa1e443f95aecd53a961ca88ed4ac9
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2024-11-29 17:23:39 +0000

    restart_course

--- final_project/server.py
+++ final_project/server.py
@@ -1,24 +1,46 @@
-from machinetranslation import translator
+"""
+Emotion Detection Server
+
+This script defines a Flask-based server for performing emotion detection on user-provided text.
+
+Author(Learner): [NoorAldin]
+"""
+
 from flask import Flask, render_template, request
-import json
+from EmotionDetection.emotion_detection import emotion_detector
+from EmotionDetection.emotion_detection import emotion_predictor
 
-app = Flask("Web Translator")
+app = Flask("Emotion Detection")
 
-@app.route("/englishToFrench")
-def englishToFrench():
-    textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to French"
+def run_emotion_detection():
+    """
+    Main function to run the Emotion Detection application.
+    """
+    app.run(host="0.0.0.0", port=5000)
 
-@app.route("/frenchToEnglish")
-def frenchToEnglish():
-    textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to English"
+@app.route("/emotionDetector")
+def sent_detector():
+    """
+    Analyze the user-provided text for emotions and return the result.
+    """
+    text_to_detect = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_detect)
+    formated_response = emotion_predictor(response)
+    if formated_response['dominant_emotion'] is None:
+        return "Invalid text! Please try again."
+    return (
+        f"For the given statement, the system response is 'anger': {formated_response['anger']} "
+        f"'disgust': {formated_response['disgust']}, 'fear': {formated_response['fear']}, "
+        f"'joy': {formated_response['joy']} and 'sadness': {formated_response['sadness']}. "
+        f"The dominant emotion is {formated_response['dominant_emotion']}."
+    )
 
 @app.route("/")
-def renderIndexPage():
-    # Write the code to render template
+def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    run_emotion_detection()
